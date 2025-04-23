7f862e9a1df08618d37b463404b6487b7ccb648e
ElloJhusthine <jhusthine.ello@gmail.com>
2025-04-24 04:00:53 +0800

    Added Render domain to ALLOWED_HOSTS

--- todo-app/backend/config/settings.py
+++ todo-app/backend/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-bfz7zcz*gvi*%o7y=)poov-4)quhuk1b$b*clrdl=$^6$c$n$d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['full-activity.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
