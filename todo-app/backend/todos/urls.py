3c19af4f844f914d110fb9b596ab3cc86bddd6de
ElloJhusthine <jhusthine.ello@gmail.com>
2025-04-25 02:04:25 +0800

    Fix missing TaskListCreateView in views

--- todo-app/backend/todos/urls.py
+++ todo-app/backend/todos/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path('tasks/', views.TaskListCreateView.as_view(), name='task-list'),
-    path('tasks/<int:pk>/', views.TaskRetrieveUpdateDestroyView.as_view(), name='task-detail'),
 ]
